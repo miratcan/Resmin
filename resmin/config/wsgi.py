28941c28a07f192345169d7b8f4493bb7855e7a8
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2013-11-24 22:05:53 +0200

    HOTFIX: wsgi.py dosyasında düzeltme.

--- resmin/config/wsgi.py
+++ resmin/config/wsgi.py
@@ -4,9 +4,10 @@ import site
 
 site.addsitedir('/home/miratcan/envs/cb2/lib/python2.7/site-packages/')
 sys.path.append("/home/miratcan/webapps/resmin/")
-sys.path.append("/home/miratcan/webapps/resmin/cb2/")
+sys.path.append("/home/miratcan/webapps/resmin/resmin/")
+sys.path.append("/home/miratcan/webapps/resmin/resmin/resmin/")
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'resmin.config.settings'
 
 import django.core.handlers.wsgi
 
