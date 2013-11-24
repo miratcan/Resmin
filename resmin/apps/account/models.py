d8976efbc13dd1250f7d6edc9a70797bad455c3b
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2013-11-24 21:58:11 +0200

    HOTFIX: answers_base.html dosyasında silinmiş javascript, User.profile monkeypatch'inin geri eklenmesi

--- resmin/apps/account/models.py
+++ resmin/apps/account/models.py
@@ -42,6 +42,10 @@ class UserProfile(models.Model):
     def get_absolute_url(self):
         reverse('profile', args=[self.user.username, ])
 
+
+User.profile = lambda u: UserProfile.objects.get_or_create(user=u)[0]
+
+
 class Invitation(models.Model):
     owner = models.ForeignKey(User)
     key = models.CharField(max_length=255, blank=True)
