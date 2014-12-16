9e5a775b04c67290a7c655c85e9aa2558fcef9d9
Mirat Can Bayrak <mirat.bayrak@markafoni.com>
2014-12-16 20:56:02 +0000

    Fixed #77

--- resmin/apps/story/forms.py
+++ resmin/apps/story/forms.py
@@ -39,7 +39,6 @@ class StoryForm(forms.ModelForm):
                         questionee=self.owner))
 
     def save_slots(self, story, slot_data):
-        import ipdb; ipdb.set_trace()
         CT_MAP = {'image': ContentType.objects.get(
             app_label='story', model='image')}
         slots = story.slot_set.all()
