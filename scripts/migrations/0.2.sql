e8b505f47bb59da79be2702eb98738e234448bac
Mirat Can Bayrak <mirat.bayrak@markafoni.com>
2015-01-25 13:44:06 +0000

    Clean ups to be open sourced.

--- scripts/migrations/0.2.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE question_answer ADD COLUMN "title" varchar(255);
-ALTER TABLE question_answer ADD COLUMN "description" text;
-ALTER TABLE question_answer ADD COLUMN "source_url" varchar(200);fa
