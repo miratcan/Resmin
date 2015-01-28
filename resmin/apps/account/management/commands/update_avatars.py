9bc33d4eb894ddc240c2a2f52d3e79b9a787faf2
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2015-01-28 12:51:17 +0000

    Fixes for update_avatars command.

--- resmin/apps/account/management/commands/update_avatars.py
+++ resmin/apps/account/management/commands/update_avatars.py
@@ -2,11 +2,12 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.conf import settings
 from utils import _set_avatar_to_answer
-
+from apps.question.models import QuestionMeta
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         for user in User.objects.all():
-            story = user.story_set.filter(question_id=settings.AVATAR_QUESTIONMETA_ID).first()
+            story = user.story_set.filter(mounted_question_metas=QuestionMeta.objects.get(id=settings.AVATAR_QUESTIONMETA_ID)).first()
             if story:
                 _set_avatar_to_answer(story)
+                print "avatar fixed for %s" % story.owner
