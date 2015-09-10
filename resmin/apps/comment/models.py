7548b2c0f833f876086c62cc3b44bc04f13b92a4
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2015-09-10 14:13:15 +0300

    Remove redundant comments.

--- resmin/apps/comment/models.py
+++ resmin/apps/comment/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 from apps.story.models import Story
 from django.template.defaultfilters import linebreaks, urlize
-# Create your models here.
 
 
 class CommentManager(models.Manager):
