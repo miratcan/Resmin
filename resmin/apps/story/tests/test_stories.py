3d6635700faafa51b6cf0a5dcc078310cc0c3fa2
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2015-11-09 18:51:50 +0200

    Added tests for story listing.

--- resmin/apps/story/tests/test_stories.py
+++ resmin/apps/story/tests/test_stories.py
@@ -7,25 +7,31 @@ from resmin.utils.factories import (
     StoryFactory, UserFactory, UserFollowFactory, QuestionMetaFactory)
 
 
-class TestStoryVisibity(TestCase):
-
+class TestStoryBase(TestCase):
     def setUp(self):
         self.u1 = UserFactory()
         self.u2 = UserFactory()
         self.u3 = UserFactory()
+        self.au = AnonymousUser()
 
         self.qm = QuestionMetaFactory(owner=self.u1)
+
+        """ Draft story. """
         self.ds = StoryFactory(
             owner=self.u1, visible_for=Story.VISIBLE_FOR_EVERYONE,
             status=Story.DRAFT, question_meta=self.qm)
+
+        """ Published story. """
         self.ps = StoryFactory(
             owner=self.u1, visible_for=Story.VISIBLE_FOR_EVERYONE,
             status=Story.PUBLISHED, question_meta=self.qm)
-        self.au = AnonymousUser()
 
         """ User 3 blocks User 1."""
         UserFollowFactory(
             status=UserFollow.BLOCKED, follower=self.u1, target=self.u1)
+
+
+class TestStoryVisibity(TestStoryBase):
 
     def test_draft_story_must_be_visible_to_owner(self):
 
@@ -53,3 +59,26 @@ class TestStoryVisibity(TestCase):
 
         """ Draft story must not be visible for a blocked user. """
         self.assertEqual(self.ds.is_visible_for(self.u3), False)
+
+
+class TestStoryListingVisibility(TestStoryBase):
+
+    def test_listing_from_question_meta(self):
+        qm = QuestionMetaFactory(owner=self.u1)
+        s2 = StoryFactory(owner=self.u1, question_meta=qm)
+        qs = Story.objects.build(frm=self.qm, listing='public')
+        self.assertEqual(s2 not in qs, True)
+
+    def test_listing_from_user(self):
+        qm = QuestionMetaFactory(owner=self.u1)
+        s2 = StoryFactory(owner=self.u1, question_meta=qm)
+        qs = Story.objects.build(frm=self.u1, listing='public')
+        self.assertEqual(qs.count(), 1)
+
+    def test_listing_from_wall(self):
+        """ Hey it's good starting point for learning TDD.
+            Be bold about sending pull request. """
+
+    def test_listing_from_wall_when_blocked_some_users(self):
+        """ Hey it's good starting point for learning TDD.
+            Be bold about sending pull request. """
